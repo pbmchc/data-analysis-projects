f874cfac085a52d8a3a6e8113694ecc56f74b2b0
pbmchc <pbmalecki@wp.pl>
2021-07-19 18:06:57 +0200

    Implemented drawing sea level prediction plot

--- 05-sea-level-predictor/sea_level_predictor.py
+++ 05-sea-level-predictor/sea_level_predictor.py
@@ -2,18 +2,51 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
+FILE_PATH = 'epa-sea-level.csv'
+YEAR_COLUMN = 'Year'
+SEA_LEVEL_COLUMN = 'CSIRO Adjusted Sea Level'
+
+
+def init_data():
+    return pd.read_csv(FILE_PATH)
+
 
 def draw_plot():
-    # Read data from file
+    df = init_data()
 
-    # Create scatter plot
+    df_scatter = df[[YEAR_COLUMN, SEA_LEVEL_COLUMN]]
+    years = df_scatter[YEAR_COLUMN]
+    sea_level = df_scatter[SEA_LEVEL_COLUMN]
 
-    # Create first line of best fit
+    plt.scatter(x=years, y=sea_level)
 
-    # Create second line of best fit
+    predictions_range = _get_predictions_range(years.max() + 1)
 
-    # Add labels and title
+    years_with_predictions, sea_level_values = _get_line_of_best_fit(df, predictions_range)
+    plt.plot(years_with_predictions, sea_level_values, color='black')
 
-    # Save plot and return data for testing (DO NOT MODIFY)
+    df_current_mill = df_scatter[df_scatter[YEAR_COLUMN] >= 2000]
+
+    years_with_predictions_current_mill, sea_level_values_current_mill = _get_line_of_best_fit(df_current_mill, predictions_range)
+    plt.plot(years_with_predictions_current_mill, sea_level_values_current_mill, color='red')
+
+    plt.title('Rise in Sea Level')
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+
     plt.savefig('sea_level_plot.png')
     return plt.gca()
+
+
+def _get_predictions_range(predictions_range_start, predictions_range_end=2050):
+    return pd.Series([year for year in range(predictions_range_start, predictions_range_end + 1)])
+
+
+def _get_line_of_best_fit(df, predictions_range):
+    years = df[YEAR_COLUMN]
+    sea_level = df[SEA_LEVEL_COLUMN]
+    result = linregress(years, sea_level)
+    years_with_predictions = years.append(predictions_range)
+    sea_level_values = result.slope * years_with_predictions + result.intercept
+
+    return years_with_predictions, sea_level_values
